993d8aedb64a86f56fc4f6999e3590df0dba0e11
Jacob Andrus <mjandrus06@gmail.com>
2021-01-03 19:50:29 -0500

    added drag and drop box that still needs work

--- Drag and Drop/drop.py
+++ Drag and Drop/drop.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 import argparse
+import os
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
+
+app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app) 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
